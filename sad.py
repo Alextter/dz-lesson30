cc7593b5fae6bb5966348c7326357905ef1431d1
Alextter <74066029+Alextter@users.noreply.github.com>
2022-01-19 18:35:32 +0200

    Add files via upload

---
+++ sad.py
@@ -0,0 +1,72 @@
+#class Rectangle:
+#    def __init__(self,a1,a2):
+#        self.a1=a1
+#        self.a2=a2
+#    def plosha(self):
+#        p1=a1*a2
+#        print(f"Площадь прямугольника равна {p1}")
+
+#a1=int(input("Введите длинну ---->>>>"))
+#a2=int(input("Введите ширину ---->>>>"))
+#p1=Rectangle(a1,a2)
+#p1.plosha()
+
+
+
+# class Cuprum():
+#   def __init__(self, input_, output):
+#     self.input = input_
+#     self.put = put
+
+#   def prints(self, y):
+#     print(y.upper())
+
+# i = input("String--->>> ")
+# p1 = Cuprum("","")
+# p1.prints(y)
+
+
+
+
+
+# class Shop():
+#   def __init__(self, shop_name, store_type):
+#     self.shop_name = shop_name
+#     self.store_type = store_type
+#     self.number_of_units = 0
+
+#   def openshop(self):
+#     print("Shop is open!")
+
+#   def describe_shop(self):
+#     print(f"Name: {self.shop_name}, type: {self.store_type}")
+
+  
+#   def set_number_of_units(self):
+#     print(f"Units: {self.number_of_units}")
+
+#   def increment_number_of_units(self, i):
+#     self.number_of_units += i
+
+# i = int(input("Set number of units: "))
+# p1 = Shop("Store", "products")
+# p1.increment_number_of_units(i)
+# p1.set_number_of_units()
+# p1.describe_shop()
+# p1.openshop()
+
+
+
+# class discont(Shop):
+#   def __init__(self, discount_product):
+#     self.discount_product = discount_product
+
+#   def get_discount_product(self):
+#     print(f"Discount product: {self.discount_product}")
+
+# store = discont("Lapsha, Manka")
+# store.get_discount_product()
+
+
+
+
